479cef33c341ae3a764093ddfd0d790111cac241
elsacodesid <elsacodesid@gmail.com>
2024-07-13 12:02:46 +0700

    update

---
+++ components/LoginScreen.tsx
@@ -0,0 +1,118 @@
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import * as WebBrowser from "expo-web-browser";
+import { useOAuth } from "@clerk/clerk-expo";
+import * as Linking from "expo-linking"
+import { Colors } from "../constants/Colors.ts";
+
+
+export const useWarmUpBrowser = () => {
+    React.useEffect(() => {
+      // Warm up the android browser to improve UX
+      // https://docs.expo.dev/guides/authentication/#improving-user-experience
+      void WebBrowser.warmUpAsync();
+      return () => {
+        void WebBrowser.coolDownAsync();
+      };
+    }, []);
+  };
+  
+  WebBrowser.maybeCompleteAuthSession();
+
+const LoginScreen = () => {
+  useWarmUpBrowser();
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow({ redirectUrl: Linking.createURL("/dashboard", { scheme: "myapp" })});
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+  return (
+    <View>
+      <View
+        style={{
+          display: "flex",
+          alignItems: "center",
+          marginTop: 100,
+        }}
+      >
+        <Image
+          source={require("../assets/images/login.png")}
+          style={{
+            width: 220,
+            height: 400,
+            borderRadius: 20,
+            borderWidth: 6,
+            borderColor: "#000",
+          }}
+        />
+      </View>
+      <View style={styles.subContainer}>
+        <Text
+          style={{
+            fontSize: 20,
+            fontFamily: "outfit-bold",
+            textAlign: "center",
+          }}
+        >
+          Your Ultimate <Text
+            style={{
+              color: Colors.PRIMARY,
+            }}
+          >
+            Community Business Directory App
+          </Text>
+        </Text>
+        <Text
+          style={{
+            fontSize: 15,
+            fontFamily: "outfit",
+            textAlign: "center",
+            marginVertical: 15,
+            color: Colors.GRAY,
+          }}
+        >
+          Find Your Favorite Business Near You and Post Your Own Business To
+          Your Community
+        </Text>
+        <TouchableOpacity onPress={onPress} style={styles.btn}>
+          <Text
+            style={{
+              textAlign: "center",
+              color: "#fff",
+              fontFamily: "outfit",
+            }}
+          >
+            Let's Get Started
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  subContainer: {
+    backgroundColor: "#fff",
+    padding: 20,
+    marginTop: -20,
+  },
+  btn: {
+    backgroundColor: Colors.PRIMARY,
+    padding: 16,
+    borderRadius: 99,
+    marginTop: 20,
+  },
+});
