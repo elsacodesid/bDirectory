ad8a9e28dd4321ca34e276b5d51e364930fe756f
elsacodesid <elsacodesid@gmail.com>
2024-07-14 23:08:37 +0700

    update

--- components/Home/BusinessCard.jsx
+++ components/Home/BusinessCard.jsx
@@ -1,32 +1,87 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
-import { Colors } from '../../constants/Colors'
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { Colors } from "../../constants/Colors";
 
-const BusinessCard = ({business}) => {
+const BusinessCard = ({ business }) => {
   return (
     <View>
-      <Image source={{uri:business?.imageUrl}} style={{
-        width: 200,
-        height: 130,
-        borderRadius: 15,
-        marginRight: 15
-      }} />
-      <View style={{
-        marginTop: 7
-      }}>
-      <Text style={{
-        fontFamily: "outfit-bold",
-        fontSize: 17,
-
-      }}>{business.name}</Text>
-      <Text style={{
-        fontFamily: "outfit",
-        fontSize: 13,
-      
-      }}>{business.address}</Text>
-</View>
+      <Image
+        source={{ uri: business?.imageUrl }}
+        style={{
+          width: 200,
+          height: 130,
+          borderRadius: 15,
+          marginRight: 15,
+        }}
+      />
+      <View
+        style={{
+          marginTop: 7,
+          gap: 5,
+        }}
+      >
+        <View style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 2
+        }}>
+        <Text
+          style={{
+            fontFamily: "outfit-bold",
+            fontSize: 17,
+            padding: 2
+          }}
+        >
+          {business.name}
+        </Text>
+        <Text style={{
+            fontFamily: "outfit",
+            backgroundColor: Colors.PRIMARY,
+            color: "#fff",
+            padding: 2,
+            fontSize: 10,
+            borderRadius: 10
+          }}>{business.category}</Text>
+        </View>
+        
+        <Text
+          style={{
+            fontFamily: "outfit",
+            fontSize: 13,
+          }}
+        >
+          {business.address}
+        </Text>
+        <View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              gap: 5,
+            }}
+          >
+            <Image
+              source={require("../../assets/images/star.png")}
+              style={{
+                width: 15,
+                height: 15,
+              }}
+            />
+            <Text
+              style={{
+                fontFamily: "outfit",
+              }}
+            >
+              4.5
+            </Text>
+            
+          </View>
+        
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default BusinessCard
+export default BusinessCard;
